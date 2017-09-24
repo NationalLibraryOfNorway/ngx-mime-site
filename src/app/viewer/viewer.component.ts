d89570bd71166d46461c0bb09178c1ff47511911
ronnymikalsen <ronny.mikalsen@nb.no>
2017-09-24 23:43:50 +0200

    fix: cleanup

--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss']
 })
-export class ViewerComponent implements OnInit {
+export class ViewerComponent implements OnInit, OnDestroy {
   public manifestUri: string;
+  private subscriptions: Array<Subscription> = [];
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(q => {
+    this.subscriptions.push(this.route.paramMap.subscribe(q => {
       this.manifestUri = q.get('manifestUri');
-    });
+    }));
   }
 
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription: Subscription) => {
+      subscription.unsubscribe();
+    });
+  }
 }
