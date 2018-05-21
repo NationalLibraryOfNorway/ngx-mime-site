758b48823daa434bc053de01d77af573ba953594
ronnymikalsen <ronny.mikalsen@nb.no>
2018-05-21 18:33:10 +0200

    feat: upgrade angular cli to 6.0.3

--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material';
+
 import { ViewerComponent } from './../viewer/viewer.component';
 
 export interface Sample {
